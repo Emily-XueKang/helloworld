d2deb3f42f44ef730c4ad7505361173616956734
Emily-Xue Kang <xkang3@dons.usfca.edu>
2017-06-25 01:10:15 -0700

    modify urls

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^welcome/', include('welcome.urls')),
+	url(r'^', include('welcome.urls')),
 	url(r'^admin/', admin.site.urls),
 ]
